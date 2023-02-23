f47886d1c4b0b86633e67bacb1db33564eb18aab
psankesh9 <69172877+psankesh9@users.noreply.github.com>
2023-02-22 18:59:01 -0800

    Adding annotations to the models file

--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
-from __future__ import division, print_function, unicode_literals
+from __future__ import division, print_function, unicode_literals 
+
+#most of the code is python2 the above line allows us to use features from python3
 
 import json
 import math
@@ -10,20 +12,30 @@ from Queue import PriorityQueue
 
 import numpy as np
 import torch
-import torch.nn as nn
+import torch.nn as nn 
 import torch.nn.functional as F
 from torch import optim
 
 import policy
 
-SOS_token = 0
-EOS_token = 1
-UNK_token = 2
-PAD_token = 3
+SOS_token = 0 #stands for start of sequence in NLP frameworks
+
+EOS_token = 1 #stands for end of sequence
+UNK_token = 2 #stands for unknown tokens
+PAD_token = 3 #its a special token that is used to pad sequences of tokens to a fixed length.
 
 
 # Shawn beam search decoding
-class BeamSearchNode(object):
+
+'''
+Given a trained model, we obtain a set of highly probable sequences. In practice, this problem is often intractable due to the size of Ω, 
+which grows exponentially in sequence length. As a result, we resort to approximating the optimization problem using a decoding algorithm
+that returns a set of k sequences F(pθ; γ), where F denotes the decoding algorithm, and γ denotes its hyper-parameters. Concretely, we consider two
+decoding approaches: a deterministic decoding algorithm that produces a set of sequences using beam search with beam-width k, and a stochastic
+decoding algorithm that forms a set of sequences using ancestral sampling until k unique sequences are obtained.1 We refer readers to Welleck et al.
+(2020a) for detailed descriptions of those decoding algorithms. 
+'''
+class BeamSearchNode(object):  #class number 1 and this is the decoder
     def __init__(self, h, prevNode, wordid, logp, leng):
         self.h = h
         self.prevNode = prevNode
@@ -37,8 +49,20 @@ class BeamSearchNode(object):
 
         return self.logp / float(self.leng - 1 + 1e-6) + alpha * reward
 
+'''
+The Long Short-Term Memory
+(LSTM) is a specific RNN (recurrent neural network) architecture whose
+design makes it much easier to train
 
-def init_lstm(cell, gain=1):
+its comparable to GRU 
+
+ Gated Recurrent Unit (GRU) is a type of Recurrent Neural Network (RNN) 
+ that, in certain cases, has advantages over long short term memory (LSTM). 
+ GRU uses less memory and is faster than LSTM, however, LSTM is more accurate when using datasets with longer sequences.
+ 
+ #so the question here is what is this sequence
+'''
+def init_lstm(cell, gain=1): #a method
     init_gru(cell, gain)
 
     # positive forget gate bias (Jozefowicz et al., 2015)
@@ -46,16 +70,85 @@ def init_lstm(cell, gain=1):
         l = len(ih_b)
         ih_b[l // 4:l // 2].data.fill_(1.0)
         hh_b[l // 4:l // 2].data.fill_(1.0)
+'''
+Chat GPT description of the above snippet of code:
+The code snippet you provided is setting a positive bias for the forget gate in the weights of a Long Short-Term Memory (LSTM) cell. 
+This technique was introduced in a paper by Jozefowicz et al. in 2015 and is known as "positive forget gate bias".
+
+In an LSTM cell, the forget gate determines how much of the previous cell state should be forgotten or retained.
+The positive forget gate bias means that the forget gate bias is set to a positive value, which biases the gate towards 
+retaining more of the previous cell state. This can help to prevent the cell from forgetting important information that was stored in the previous time step.
+
+The code sets the bias for the input-hidden and hidden-hidden weight matrices of the LSTM cell by filling a slice 
+of the weights with the value 1.0. Specifically, it sets the bias for the forget gate weights to 1.0 for the input-hidden 
+and hidden-hidden weight matrices. The slice of the weight matrix that is being modified is determined by the length of the 
+input-hidden bias weights (len(ih_b)) and the fact that the LSTM weights are structured into four parts (input gate, forget gate, output gate, and cell input).
+The slice for the forget gate weights corresponds to the second quarter of the weights (from l // 4 to l // 2).
+'''
 
 
-def init_gru(gru, gain=1):
+
+
+'''
+The code you provided is a function for initializing the weights of a Gated Recurrent Unit (GRU) neural network layer in PyTorch.
+
+The function takes a GRU layer as input and an optional gain parameter, which defaults to 1. 
+It first resets the parameters of the GRU layer to their default values. 
+Then, it iterates over the weights of the GRU layer and initializes the hidden-to-hidden weight matrix 
+using the orthogonal initialization method (torch.nn.init.orthogonal_()).
+
+The hidden-to-hidden weight matrix is divided into two parts: the reset gate and the update gate. 
+Each part has a size of gru.hidden_size, which is the number of hidden units in the GRU layer. 
+The loop iterates over each reset/update gate weight matrix in steps of gru.hidden_size 
+(i.e., over each row of the hidden-to-hidden weight matrix), and initializes each gate weight matrix using orthogonal initialization.
+
+Orthogonal initialization initializes the weight matrix with a random orthogonal matrix, 
+which helps to prevent the gradients from exploding or vanishing during training. 
+The gain parameter scales the orthogonal matrix by the given gain factor, which can help to improve the performance of the GRU layer.
+
+Overall, this function initializes the weights of a GRU layer in a way that helps to prevent the gradients from vanishing or exploding, 
+which can lead to better training and improved performance.
+
+
+
+
+'''
+
+def init_gru(gru, gain=1): #method 2
     gru.reset_parameters()
     for _, hh, _, _ in gru.all_weights:
         for i in range(0, hh.size(0), gru.hidden_size):
             torch.nn.init.orthogonal_(hh[i:i+gru.hidden_size],gain=gain)
 
+'''
+this is the definitionn structure
 
-def whatCellType(input_size, hidden_size, cell_type, dropout_rate):
+and nn refers to the below
+
+import torch.nn as nn 
+
+and is a neural network
+
+
+
+the bidirectional parameter is also useful
+
+The bidirectional parameter is an optional parameter in many PyTorch recurrent neural network (RNN) layers,
+such as LSTM and GRU. When set to True, it enables the RNN layer to process the input sequence in both forward and backward directions.
+
+By default, an RNN layer processes the input sequence in a single direction, either from the first 
+time step to the last time step (i.e., forward direction) or from the last time step to the first time step (
+i.e., backward direction). However, in many applications, information from both directions can be useful for 
+predicting the output sequence. For example, in natural language processing, the meaning of a word in a sentence 
+can depend on the words that come both before and after it.
+
+By setting the bidirectional parameter to True, the RNN layer processes the input sequence in both directions, 
+by creating two separate hidden states for each time step, one for the forward direction and one for the backward direction. 
+The outputs from the forward and backward directions are then concatenated to produce the final output for each time step. 
+This can improve the performance of the RNN layer by allowing it to capture both past and future context of the input sequence. 
+However, it also increases the number of parameters and computation required by the RNN layer.
+'''
+def whatCellType(input_size, hidden_size, cell_type, dropout_rate): #method 3 that uses the previous methods
     if cell_type == 'rnn':
         cell = nn.RNN(input_size, hidden_size, dropout=dropout_rate, batch_first=False)
         init_gru(cell)
@@ -77,8 +170,30 @@ def whatCellType(input_size, hidden_size, cell_type, dropout_rate):
         init_lstm(cell)
         return cell
 
+'''
+The above code defines an encoder recurrent neural network (RNN) module in PyTorch.
 
-class EncoderRNN(nn.Module):
+The EncoderRNN class inherits from the nn.Module class and overrides its __init__ and forward methods.
+
+In the __init__ method, the class initializes the encoder RNN by specifying the input size, 
+embedding size, hidden size, cell type (e.g., GRU, LSTM), depth (number of layers), and dropout rate. 
+It then creates an embedding layer to convert the input sequences into dense vector representations, 
+and a recurrent layer using the specified cell type.
+
+In the forward method, the encoder takes as input a batch of input sequences (input_seqs) of variable length, 
+along with the lengths of each sequence (input_lens). The input sequences are first sorted by length in decreasing order, 
+and then embedded using the embedding layer. The resulting embeddings are packed into a PackedSequence object and fed 
+into the recurrent layer. The outputs and hidden states of the recurrent layer are then returned, with 
+the outputs optionally processed to remove the effect of bidirectionality.
+
+The purpose of the encoder RNN is to encode the input sequence into a fixed-length vector 
+representation that can be used by a decoder RNN to generate the output sequence. 
+The encoder achieves this by processing the input sequence one element at a time, 
+and updating its internal hidden state at each time step based on the current input 
+and the previous hidden state. The final hidden state of the encoder represents a summary 
+of the entire input sequence, and is used as the initial hidden state of the decoder.
+'''
+class EncoderRNN(nn.Module): #class 2 and this is the encoder
     def __init__(self, input_size,  embedding_size, hidden_size, cell_type, depth, dropout):
         super(EncoderRNN, self).__init__()
         self.input_size = input_size
@@ -129,8 +244,67 @@ class EncoderRNN(nn.Module):
             hidden = hidden.transpose(0, 1)[unsort_idx].transpose(0, 1).contiguous()
 
         return outputs, hidden
+'''
+A sequence model:
+
+A sequence-to-sequence (Seq2Seq) model is a type of neural network architecture designed for handling sequences of different lengths,
+and is often used for tasks such as machine translation, speech recognition, and text summarization. 
+The Seq2Seq model consists of two parts: an encoder network that processes the input sequence and converts
+it into a fixed-length vector representation, and a decoder network that uses the encoded vector to generate an output sequence.
+
+The encoder network can be any type of recurrent neural network (RNN), 
+such as a long short-term memory (LSTM) or gated recurrent unit (GRU), 
+and its output is a vector that summarizes the input sequence.
+This vector is then fed into the decoder network, which is typically another RNN, 
+that generates the output sequence one element at a time.
+At each time step, 
+the decoder uses the previously generated output (or a special start-of-sequence token at the first step)
+and the encoded input vector to produce the next output element. 
+The training objective is to minimize the difference between the generated output sequence and the target output sequence,
+often using a loss function like cross-entropy.
 
 
+
+
+Based off the previous statements like the above everything feels pretty standard for this model except for the decoder whihc is a beam
+search node decoder. We are trying to go and check which is teh best type of model. I dont think we have got to the loss function yet
+'''
+
+'''
+This is a PyTorch implementation of an Attention mechanism for use in a sequence-to-sequence model.
+
+The class Attn takes in two arguments, method and hidden_size, where method is a string specifying the type of attention to use 
+(e.g. "dot", "general", "concat"), and hidden_size is the size of the hidden state in the encoder and decoder.
+
+The forward method takes in two inputs, hidden and encoder_outputs, 
+where hidden is the previous hidden state of the decoder and encoder_outputs are the encoder outputs from the Encoder. 
+The method then computes the attention score between the hidden state and the encoder_outputs using the score method.
+
+The score method concatenates the hidden state and encoder_outputs, 
+applies a linear transformation (attn), applies a hyperbolic tangent function (tanh) 
+and a dot product between the result and a learnable parameter v (self.v). 
+The final result is then returned as the attention energies.
+
+'''
+'''
+The attention mechanism in sequence-to-sequence (seq2seq) models is a technique that helps the decoder 
+focus on specific parts of the input sequence when generating each output token.
+
+In a traditional seq2seq model, the entire input sequence is encoded into a fixed-length vector, 
+which is then fed into the decoder to generate the output sequence. However, 
+this approach can be limiting because the fixed-length vector has to capture all the information in the input sequence,
+and may not be able to handle longer sequences or more complex relationships between the input and output.
+
+The attention mechanism addresses this issue by allowing the decoder to selectively focus on different parts of the input sequence at each time step. 
+Specifically, at each step of decoding, the attention mechanism computes a set of weights that indicate how much attention to give to each input element.
+These weights are then used to compute a weighted sum of the input elements, 
+which is combined with the current hidden state of the decoder to generate the output token.
+
+By allowing the decoder to selectively focus on different parts of the input sequence, 
+the attention mechanism can improve the accuracy of seq2seq models, 
+particularly for longer sequences or more complex relationships between the input and output.
+
+'''
 class Attn(nn.Module):
     def __init__(self, method, hidden_size):
         super(Attn, self).__init__()
@@ -165,7 +339,27 @@ class Attn(nn.Module):
         energy = torch.bmm(v,energy)  # [B*1*T]
         return energy.squeeze(1)  # [B*T]
 
+'''
+This is a decoder module in a sequence-to-sequence model that uses attention mechanism. 
+It takes as input the target token (in the form of an index) and the previous hidden state of the decoder, 
+and returns the output probability distribution over the target vocabulary and the new hidden state of the decoder.
 
+The attention mechanism is used to compute a context vector that summarizes the relevant parts of the encoder's outputs 
+for predicting the next target token. It does this by computing a score for each encoder output based on how well it 
+matches the current decoder hidden state, and then using these scores to compute a weighted sum of the encoder outputs,
+where the weights are given by the softmax of the scores.
+
+The attention weights and the context vector are computed using the following steps:
+
+Concatenate the current decoder hidden state with the encoder outputs along the time dimension
+Apply a linear layer to this concatenated tensor to obtain a score for each encoder output
+Apply the softmax function to these scores to obtain the attention weights
+Compute the weighted sum of the encoder outputs using these attention weights to obtain the context vector.
+The decoder then concatenates the context vector with the embedded input token, and feeds it to an RNN 
+(whose type is specified by the cell_type argument) along with the previous decoder hidden state, 
+to obtain the new decoder hidden state and the output probability distribution over the target vocabulary. 
+The output probability distribution is obtained by applying a linear layer followed by a log-softmax activation function to the output of the RNN.
+'''
 class SeqAttnDecoderRNN(nn.Module):
     def __init__(self, embedding_size, hidden_size, output_size, cell_type, dropout_p=0.1, max_length=30):
         super(SeqAttnDecoderRNN, self).__init__()
